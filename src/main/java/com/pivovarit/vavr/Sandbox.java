368c130279ec85edc7c4d85d4581c0f08a5e5ae3
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-06-15 19:50:44 +0200

    Initial

---
+++ src/main/java/com/pivovarit/vavr/Sandbox.java
@@ -0,0 +1,13 @@
+package com.pivovarit.vavr;
+
+
+/**
+ * @author Grzegorz Piwowarek
+ * @email gpiwowarek@gmail.com
+ * @twitter/github pivovarit
+ */
+public class Sandbox {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/com/pivovarit/vavr/VavrTest.java
@@ -0,0 +1,11 @@
+package com.pivovarit.vavr;
+
+
+/**
+ * @author Grzegorz Piwowarek
+ * @email gpiwowarek@gmail.com
+ * @twitter/github pivovarit
+ */
+public class VavrTest {
+
+}
